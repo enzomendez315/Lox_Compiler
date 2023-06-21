835316a8da0a871e0b56350a541b4225446ecb3e
Enzo Mendez <enzo.mendez315@gmail.com>
2023-06-21 09:49:13 -0600

    Added methods to Parser.

--- src/lox_interpreter_java/Parser.java
+++ src/lox_interpreter_java/Parser.java
@@ -66,6 +66,18 @@ public class Parser
     }
 
     /*
+     * Checks that the next token is of the expected type. If so, it 
+     * consumes it. Otherwise, it throws and error.
+     */
+    private Token consume(TokenType type, String message)
+    {
+        if (check(type))
+            return advance();
+
+        throw error(peek(), message);
+    }
+
+    /*
      * Returns true if the current token is of the given type. False otherwise.
      * Unlike match(), it looks at the token but does not consume it.
      */
@@ -115,6 +127,16 @@ public class Parser
     /*
      * 
      */
+    private ParseError error(Token token, String message)
+    {
+        Lox.error(token, message);
+
+        return new ParseError();
+    }
+
+    /*
+     * 
+     */
     private Expr comparison()
     {
         Expr expr = term();
@@ -127,5 +149,80 @@ public class Parser
         }
 
         return expr;
+    }
+
+    /*
+     * 
+     */
+    private Expr term()
+    {
+        Expr expr = factor();
+
+        while (match(TokenType.MINUS, TokenType.PLUS))
+        {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    /*
+     * 
+     */
+    private Expr factor()
+    {
+        Expr expr = unary();
+
+        while (match(TokenType.SLASH, TokenType.STAR))
+        {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    /*
+     * 
+     */
+    private Expr unary()
+    {
+        if (match(TokenType.BANG, TokenType.MINUS))
+        {
+            Token operator = previous();
+            Expr right = unary();
+
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    /*
+     * 
+     */
+    private Expr primary()
+    {
+        if (match(TokenType.FALSE))
+            return new Expr.Literal(false);
+
+        if (match(TokenType.TRUE))
+            return new Expr.Literal(true);
+
+        if (match(TokenType.NIL))
+            return new Expr.Literal(null);
+
+        if (match(TokenType.NUMBER, TokenType.STRING))
+            return new Expr.Literal(previous().literal);
+
+        if (match(TokenType.LEFT_PAREN))
+        {
+            Expr expr = expression();
+            consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
     }
 }
